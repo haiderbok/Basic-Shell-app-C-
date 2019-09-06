f8fae7a0571ca4deccaea10e2e61698655bf3fc7
haiderbok <54965588+haiderbok@users.noreply.github.com>
2019-09-06 18:24:25 -0400

    Create fish.c

---
+++ fish.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+        #include<stdlib.h>
+        #include<unistd.h>
+        #include<sys/types.h>
+        #include<sys/wait.h>
+        #include<string.h>
+
+        int j=2;
+        void input();
+        int main(){
+//Declaring variables 
+        input();
+        }
